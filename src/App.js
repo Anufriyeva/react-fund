b352ef197d804cee213a062915e8a8c40417821d
Evgeniya Anufriyeva <2281474@gmail.com>
2023-06-20 19:26:46 +0300

    commit3

--- src/App.js
+++ src/App.js
@@ -2,29 +2,27 @@ import React, { useState } from "react";
 import Counter from "./components/Count";
 import ClassCounter from "./components/ClassCount";
 import "./styles/App.css";
+import PostItem from "./components/PostItem";
 
 
 function App() {
 
-  const [value, setValue] = useState("Input text")
+  const [posts, setPosts] = useState([
+    { id: 1, title: "Javascript", body: "Description" },
+  { id: 1, title: "Javascript", body: "Description" },
+  { id: 2, title: "Javascript 2", body: "Description" },
+  ])
   
   
     
   return (
     <div className="App">
-      <div className="post">
-        <div className="post__content">
-          <strong>1. Javascript</strong>
-          <div>
-            Javascript - язык программирования
-          </div>
-        </div>
-        <div className="post__btns">
-          <button>Удалить</button>
-
-        </div>
-
-      </div>
+      <h1 style={{ textAlign: "center" }}>
+        Список постов
+      </h1>
+      {posts.map(post => 
+        <PostItem post={post} key={post.id} />
+      )}
       
     </div>
   );
