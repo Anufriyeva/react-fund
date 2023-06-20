e03ad3e06ea8ec8373c8fe53ec3a09bd8036a0c7
Evgeniya Anufriyeva <2281474@gmail.com>
2023-06-20 17:00:45 +0300

    commit2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import Counter from "./components/Count";
+import ClassCounter from "./components/ClassCount";
+import "./styles/App.css";
+
 
 function App() {
 
@@ -9,7 +12,19 @@ function App() {
     
   return (
     <div className="App">
-      <Counter/>
+      <div className="post">
+        <div className="post__content">
+          <strong>1. Javascript</strong>
+          <div>
+            Javascript - язык программирования
+          </div>
+        </div>
+        <div className="post__btns">
+          <button>Удалить</button>
+
+        </div>
+
+      </div>
       
     </div>
   );
