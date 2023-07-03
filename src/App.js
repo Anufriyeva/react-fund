223b09784a367b2912aa9426b3f7a412e66ef0f8
Evgeniya Anufriyeva <2281474@gmail.com>
2023-07-03 14:30:09 +0300

    commit16

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import axios from "axios";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader";
 import { useFetching } from "./hooks/useFetching";
+import { getPageCount, getPagesArray } from "./utils/pages";
 
 
 function App() {
@@ -22,22 +23,24 @@ function App() {
   const [posts, setPosts] = useState([])  
   const [filter, setFilter] = useState({ sort: '', query: '' })  
   const [modal, setModal] = useState(false);
-  const [totalCount, setTotalCount] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
   const [limit, setLimit] = useState(10);
   const [page, setPage] = useState(1);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
+  let pagesArray = getPagesArray(totalPages);
   
-  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
     const response = await PostService.getAll(limit, page);
     setPosts(response.data)
-    console.log(response.headers['x-total-count']);
-    setTotalCount(response.headers['x-total-count'])
+    const totalCount = response.headers['x-total-count'];
+    setTotalPages(getPageCount(totalCount, limit));
   })
+  console.log(totalPages);
 
   useEffect(() => {
-    fetchPosts()
-
-  }, [filter])
+    fetchPosts(limit, page)
+  }, [])
   
    
   
@@ -50,8 +53,13 @@ function App() {
   // Получаем post из дочернего компонента
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
-
   }
+
+  const changePage = (page) => {
+    setPage(page)
+    fetchPosts(limit, page)
+  }
+    
     
   return (
     <div className="App">
@@ -76,7 +84,17 @@ function App() {
       {isPostsLoading
         ? <div style={{display: "flex", justifyContent: "center", marginTop: 50}}><Loader/></div>
         : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS" />
-      }     
+      }  
+      <div className="page__wrapper">
+        {pagesArray.map(p =>
+          <span
+            onClick={() => changePage(p)}
+            key={p}
+            className={page === p ? 'page page__current' : 'page'}>
+            {p}
+          </span>)}
+      </div>
+      
        
     </div>
   );
