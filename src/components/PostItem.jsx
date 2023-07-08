490acd7d320727a8efb8c6c65f4a3b2a238342ed
Evgeniya Anufriyeva <2281474@gmail.com>
2023-07-08 16:15:18 +0300

    commit-19

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import MyButton from "./UI/MyButton";
+import { useNavigate } from "react-router-dom";
 
 const PostItem = (props) => {
+  const navigate = useNavigate()
 
     return (
         <div className="post">
@@ -12,7 +14,8 @@ const PostItem = (props) => {
           </div>
         </div>
         <div className="post__btns">
-            <MyButton onClick={() => props.remove(props.post)}>Удалить</MyButton>
+          <MyButton onClick={() => navigate(`/posts/${props.post.id}`)}>Open</MyButton>
+          <MyButton onClick={() => props.remove(props.post)}>Delete</MyButton>
         </div>
         </div>
     );
